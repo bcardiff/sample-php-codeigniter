57458b217ef89c5bfb41ffd4e6695ca5fb5d5128
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-04-04 23:19:52 +0300

    Add migration support

---
+++ application/controllers/Migration.php
@@ -0,0 +1,25 @@
+<?php
+
+class Migration extends CI_Controller
+{
+
+  public function __construct()
+  {
+    parent::__construct();
+    if (!$this->input->is_cli_request())
+      show_404();
+
+    $this->load->library("migration");
+    $this->load->dbforge();
+  }
+
+  public function latest()
+  {
+    if ($this->migration->current() === FALSE) {
+      echo $this->migration->error_string();
+    }
+
+    $this->migration->latest();
+    echo $this->migration->error_string();
+  }
+}
