0f0d30da02ec8eac7ccbd580fc01a4d179f82974
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-04-05 10:12:47 +0300

    add travis.yml allow DB_USER env variable to override default database user in the connection honor DB_USER in ./bin/test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
-mysql -u root -e "DROP DATABASE IF EXISTS sacids_app1_test;"
-mysql -u root -e "CREATE DATABASE sacids_app1_test;"
+DB_USER=${DB_USER:-root}
+
+mysql -u $DB_USER -e "DROP DATABASE IF EXISTS sacids_app1_test;"
+mysql -u $DB_USER -e "CREATE DATABASE sacids_app1_test;"
 CI_ENV=testing php index.php migration latest
 cd application/tests/
 ../../bin/phpunit
