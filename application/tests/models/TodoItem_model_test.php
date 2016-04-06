194a7b9d49739ba4697b9a952ad546e612ddbec2
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-04-06 13:05:48 +0300

    add model test units

---
+++ application/tests/models/TodoItem_model_test.php
@@ -0,0 +1,26 @@
+<?php
+
+class TodoItem_model_test extends TestCase
+{
+  public function setUp() {
+    $this->resetInstance();
+    $this->CI->load->database();
+    $this->CI->db->query('DELETE FROM todo_items');
+    $this->CI->load->model('TodoItem_model');
+  }
+
+  public function test_new_items_are_pending()
+  {
+    $this->assertEquals(0, count($this->CI->TodoItem_model->get_pending()));
+    $this->CI->TodoItem_model->create('Something to do');
+    $this->assertEquals(1, count($this->CI->TodoItem_model->get_pending()));
+  }
+
+  public function test_done_items_are_not_pending()
+  {
+    $this->assertEquals(0, count($this->CI->TodoItem_model->get_pending()));
+    $id = $this->CI->TodoItem_model->create('Something to do');
+    $this->CI->TodoItem_model->done($id);
+    $this->assertEquals(0, count($this->CI->TodoItem_model->get_pending()));
+  }
+}
